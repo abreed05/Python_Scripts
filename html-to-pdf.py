3ee21e286f07dbd8536159fc1afed94264b24c59
Aaron Breeden <39411883+abreed05@users.noreply.github.com>
2020-05-15 13:23:23 -0400

    refactored code
    code not only converts the HTML file to pdf it also has built-in checking to make sure the files don't already exist.

--- html-to-pdf.py
+++ html-to-pdf.py
@@ -1,47 +1,56 @@
-# Import all needed libraries 
+# Import all needed libraries, OS, Glob, BeautifulSoup, PDFKit
 import os
 import glob
 from bs4 import BeautifulSoup
 import pdfkit
 
-
-# For loop to iterate over all files in the specified directory
-
-for filename in glob.glob("//path/to/html/files/*.htm"):
-
-    # Begin parsing HTML files using BeautifulSoup
-    
-    f = open(filename, "r")
-    contents = f.read()
-    soup = BeautifulSoup(contents, 'lxml')
-    
-    # Filter HTML Doc to find Control number 
-    
-    div = soup.find('div', class_='returns-middle')
-    td_list = div.find_all("td")
-    
-    # Variables for naming converted HTML files 
-    
-    newName = td_list[1].text
-    
-    # Change directory to save the converted HTML file
-    
-    os.chdir("\\\\path\\to\\save\\PDFs")  
-    pdfname = "Naming scheme#" + newName + ".pdf"
+# pdfConversion function. Code used to actually convert the HTML File 
+def pdfConversion():
+	os.chdir("Windows\\Path\\To\\Where\\To\\Create\\PDF")  
+	pdfname = "DSW PO# " + newName + ".pdf"
     
     # Set options for PDF conversion
-    options = {
-    'zoom': '1.10',
-    'margin-top': '0.50in',
-    'margin-right': '0.50in',
-    'margin-bottom': '0.50in',
-    'margin-left': '0.50in',
-    }
+	options = {
+	'zoom': '1.10',
+	'margin-top': '0.50in',
+	'margin-right': '0.50in',
+	'margin-bottom': '0.50in',
+	'margin-left': '0.50in',
+	}
     
-    # Begin logic to determine if file exists
+    # Begin logic to determin if file exists
     
-    if os.path.isfile(str(pdfname)):
-        print(pdfname + "exists")
+	if os.path.isfile(str(pdfname)):
+		print(pdfname + " exists")
             
-    else:
-        pdfkit.from_file(str(filename), str(pdfname), options=options)
+	else:
+		pdfkit.from_file(str(filename), str(pdfname), options=options)
+
+# For loop to iterate over each html file specified in the glob filepath
+
+for filename in glob.glob("Windows/Path/To'html_files/*.htm"):
+	# open file for reading
+	with open(filename, "r") as f:
+		contents = f.read()
+	# Convert html to BeautifulSoup Object
+	soup = BeautifulSoup(contents, 'lxml')
+	# Filter through HTML Soup object to find PO#
+	divTag = soup.find_all('div', class_='returns-middle')
+	# Create empty list
+	pos = []
+	# for loop to find all matching TD's within the div with class of returns-middle
+	for tag in divTag:
+		tdTags = tag.find_all("td")[1]
+		# For loop to add found td's to an empty list called pos
+		for tag in tdTags:
+			pos.append(tag)
+	# check length of list, if greater than or equal to 2 follow if branch 
+	x = len(pos)
+	if x >= 2:
+		for item in pos:
+			newName = item
+			pdfConversion()
+
+	else:
+		newName = tdTags.text
+		pdfConversion()
