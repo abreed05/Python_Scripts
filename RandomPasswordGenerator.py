bde1d348051b686a6393180d20c98920fb029671
aaron breeden <aaron@aarons-iMac.local>
2021-03-28 11:19:15 -0400

    created basic random password generator tool

--- RandomPasswordGenerator.py
+++ RandomPasswordGenerator.py
@@ -1,16 +1,25 @@
+#!/usr/bin/python
 import random
+import string
+import argparse
 
-allLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-allUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-allNumbers = [0,1,2,3,4,5,6,7,8,9]
-allSymbols = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","{","[","]","\\","|",",",":",";","?","/",">",".","<","`","~","\"","}"," "]
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--passwordLength', type=int, required=True, help="Number of digits to generate for password. Must be a number")
+args = parser.parse_args()
 
 
+all_lowercase_chars = string.ascii_lowercase
+all_uppercase_chars = string.ascii_uppercase
+all_digits = string.digits
+all_symbols = "~`!@#$%^&*()_-+=|\{\}\\[]/?.>,<"
+
+
+# Creates a random password based on length and the 4 criteria lowercase, uppercase, numbers, and symbols. 
 def createRandomPass():
-    gennum = input("Please enter the number of characters for your password: ")
-    randpass = random.sample(allLowerCase + allUpperCase + allNumbers + allSymbols, int(gennum))
-    randpassstrings = [str(x) for x in randpass]
-    l = ''.join(randpassstrings)
-    print(l)
+    # pass_length = int(input("Please enter the number of characters for your password: "))
+    create_password_list = random.sample(all_lowercase_chars + all_uppercase_chars + all_digits + all_symbols, args.passwordLength)
+    convert_password_list_to_string = [str(x) for x in create_password_list]
+    create_random_password = ''.join(convert_password_list_to_string)
+    print(create_random_password)
 
 createRandomPass()
